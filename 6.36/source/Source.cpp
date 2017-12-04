35e7defa1a2c776508e04e2b7fe017b1987b5e25
jo314652 <jo314652@gmail.com>
2017-12-05 00:27:18 +0800

    stv

---
+++ 6.36/source/Source.cpp
@@ -0,0 +1,28 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+void stringReverse(char data[]);
+char strings[] = " Steven is handsome";
+int i = 0;
+int main(void)
+{
+
+	i = sizeof(strings)-1;
+	for (int x = 0; x < i; x++)
+	{
+		printf("%c", strings[x]);
+	}
+	printf("\n");
+	stringReverse(strings);
+	printf("\n");
+	system("pause");
+	return 0;
+}
+
+void stringReverse(char data[]){
+	printf("%c", strings[i]);
+	if (i != 0){
+		i--;
+		stringReverse(strings);
+	}
+}
